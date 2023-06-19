5df35171f2a59d96649c0da184b14f824a7efb4f
Aditya Yadav <ishqaddy@gmail.com>
2023-06-19 17:29:07 +0530

    create twitter.js

--- apps/index.js
+++ apps/index.js
@@ -1,9 +1,11 @@
 import youtube from "./youtube.js";
 import instagram from "./instagram.js";
 import spotify from "./spotify.js";
+import twitter from "./twitter.js";
 
 export default {
   youtube,
   instagram,
-  spotify
+  spotify,
+  twitter
 };
---
+++ apps/twitter.js
@@ -0,0 +1,24 @@
+import getApp from "../helpers/getApp.js"
+
+export default (url) => {
+  let appDetails = getApp(url);
+  let { pathname, searchParams } = new URL(appDetails.url);
+  let intent_url_android = (appDetails.url.split("//")[1]).endsWith("/") ? (appDetails.url.split("//")[1]) : `${(appDetails.url.split("//")[1])}/`;
+  let intent_url_ios = "timeline"; // if nothing matches show the timeline
+  console.log(pathname);
+  if (pathname.includes("/status/")) {
+    let id = pathname.split("/status/")[1];
+    intent_url_ios = `status?id=${id}`
+  } else if (pathname.includes("/search")) {
+    let params = new URLSearchParams(searchParams);
+    intent_url_ios = `search?query=${params.get("q")}`;
+  }
+  return {
+    appName: appDetails.name,
+    intents: {
+      android: `intent://${intent_url_android}#Intent;package=${appDetails.packages.android};scheme=https;end`,
+      ios: `${appDetails.packages.ios}://${intent_url_ios}`,
+      web: url
+    }
+  }
+}
